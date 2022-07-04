06ef0be9b1d3c644014db23906865b764e8f8df0
Зендељи Шпреса <shpresa.zendelji@students.finki.ukim.mk>
2022-07-04 14:11:54 +0200

    Deleted unnecessary components

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import About from "../views/AboutView.vue";
 
 Vue.use(VueRouter);
 
@@ -10,14 +9,6 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: About,
   },
 ];
 
