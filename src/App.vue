e3b5792c93e56b532aaeb5a38e3f852fa6fa06ac
Зендељи Шпреса <shpresa.zendelji@students.finki.ukim.mk>
2022-07-05 21:40:50 +0200

    Finished LanguageProgress component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,13 @@
   <div id="app">
     <Navbar />
     <HeaderInfos />
-    <ContentCV />
+    <LanguageProgress title="English" progress="75" />
 
     <!--    <ContentCV />-->
   </div>
 </template>
 <script>
+import LanguageProgress from "./components/LanguageProgress.vue";
 import ContentCV from "./components/ContentCV.vue";
 import HeaderInfos from "./components/HeaderInfos.vue";
 import Navbar from "./components/layout/Navbar.vue";
@@ -17,6 +18,7 @@ export default {
     Navbar,
     HeaderInfos,
     ContentCV,
+    LanguageProgress,
   },
   // ...
 };
