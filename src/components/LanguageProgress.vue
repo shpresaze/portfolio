728980754c5ae9efa7a1370fb00afeeb28b9c595
shpresaze <84670618+shpresaze@users.noreply.github.com>
2022-07-05 21:52:12 +0200

    Finished LanguageProgress component (#1)

---
+++ src/components/LanguageProgress.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>
+      <strong> {{ title }}: </strong>
+    </p>
+    <div class="progress">
+      <div
+        class="progress-bar bg-success"
+        role="progressbar"
+        :style="styleObject"
+        :aria-valuenow="progress"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    progress: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      styleObject: {
+        width: this.progress + "%",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
